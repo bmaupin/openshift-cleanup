db301145d7a961734aa3b608d82a54a7fda0a165
bmaupin <bmaupin@users.noreply.github.com>
2019-02-14 14:16:40 -0500

    Add logic for cleaning deployment configs

--- openshift-template-cleanup.go
+++ openshift-template-cleanup.go
@@ -46,11 +46,14 @@ func cleanTemplateObjects(template map[interface{}]interface{}) map[interface{}]
 		case "BuildConfig":
 			object = cleanBuildConfig(object)
 			newTemplateObjects = append(newTemplateObjects, object)
+		case "DeploymentConfig":
+			object = cleanDeploymentConfig(object)
+			newTemplateObjects = append(newTemplateObjects, object)
 		case "ImageStream":
 			object = cleanImageStream(object)
 			newTemplateObjects = append(newTemplateObjects, object)
 
-		// TODO: handle DeploymentConfig, Route, Service, ...
+		// TODO: handle Route, Service, ...
 
 		// Builds will be recreated by the BuildConfig
 		case "Build":
@@ -118,6 +121,103 @@ func cleanBuildConfigSpec(buildConfig map[interface{}]interface{}) map[interface
 	return buildConfig
 }
 
+func cleanDeploymentConfig(deploymentConfig map[interface{}]interface{}) map[interface{}]interface{} {
+	deploymentConfig = cleanTemplateObject(deploymentConfig)
+	deploymentConfigSpec := deploymentConfig["spec"].(map[interface{}]interface{})
+	deploymentConfigSpec = cleanDeploymentConfigSpec(deploymentConfigSpec)
+
+	// TODO: Could we just delete the status from all template objects?
+	delete(deploymentConfig, "status")
+
+	return deploymentConfig
+}
+
+func cleanDeploymentConfigSpec(deploymentConfigSpec map[interface{}]interface{}) map[interface{}]interface{} {
+	// TODO: Verify this is the default
+	deleteKeyIfValueMatches(deploymentConfigSpec, "revisionHistoryLimit", 10)
+
+	deploymentStrategy := deploymentConfigSpec["strategy"].(map[interface{}]interface{})
+	deploymentStrategy = cleanDeploymentStrategy(deploymentStrategy)
+
+	deploymentTemplate := deploymentConfigSpec["template"].(map[interface{}]interface{})
+	deploymentTemplate = cleanDeploymentTemplate(deploymentTemplate)
+
+	// TODO: Verify this is the default
+	deleteKeyIfValueMatches(deploymentConfigSpec, "test", false)
+
+	deploymentTriggers := deploymentConfigSpec["triggers"].([]interface{})
+	for _, deploymentTrigger := range deploymentTriggers {
+		deploymentTrigger := deploymentTrigger.(map[interface{}]interface{})
+		deploymentTrigger = cleanDeploymentTrigger(deploymentTrigger)
+	}
+
+	return deploymentConfigSpec
+}
+
+func cleanDeploymentStrategy(deploymentStrategy map[interface{}]interface{}) map[interface{}]interface{} {
+	// TODO: Verify this is the default
+	deleteKeyIfValueMatches(deploymentStrategy, "activeDeadlineSeconds", 21600)
+	deleteKeyIfEmpty(deploymentStrategy, "resources")
+
+	if val, ok := deploymentStrategy["rollingParams"]; ok {
+		rollingParams := val.(map[interface{}]interface{})
+		// TODO: Verify all of these are the default
+		deleteKeyIfValueMatches(rollingParams, "intervalSeconds", 1)
+		deleteKeyIfValueMatches(rollingParams, "maxSurge", "25%")
+		deleteKeyIfValueMatches(rollingParams, "maxUnavailable", "25%")
+		deleteKeyIfValueMatches(rollingParams, "timeoutSeconds", 600)
+		deleteKeyIfValueMatches(rollingParams, "updatePeriodSeconds", 1)
+	}
+	deleteKeyIfEmpty(deploymentStrategy, "rollingParams")
+
+	return deploymentStrategy
+}
+
+func cleanDeploymentTemplate(deploymentTemplate map[interface{}]interface{}) map[interface{}]interface{} {
+	deploymentTemplate = cleanMetadata(deploymentTemplate)
+	podSpec := deploymentTemplate["spec"].(map[interface{}]interface{})
+	podSpec = cleanPodSpec(podSpec)
+
+	return deploymentTemplate
+}
+
+func cleanPodSpec(podSpec map[interface{}]interface{}) map[interface{}]interface{} {
+	containers := podSpec["containers"].([]interface{})
+	for _, container := range containers {
+		container := container.(map[interface{}]interface{})
+		container = cleanContainer(container)
+	}
+
+	deleteKeyIfValueMatches(podSpec, "dnsPolicy", "ClusterFirst")
+	deleteKeyIfValueMatches(podSpec, "restartPolicy", "Always")
+	deleteKeyIfValueMatches(podSpec, "schedulerName", "default-scheduler")
+	deleteKeyIfEmpty(podSpec, "securityContext")
+	deleteKeyIfValueMatches(podSpec, "terminationGracePeriodSeconds", 30)
+
+	return podSpec
+}
+
+func cleanContainer(container map[interface{}]interface{}) map[interface{}]interface{} {
+	// TODO: Verify this is the default
+	deleteKeyIfValueMatches(container, "imagePullPolicy", "Always")
+	deleteKeyIfEmpty(container, "resources")
+	deleteKeyIfValueMatches(container, "terminationMessagePath", "/dev/termination-log")
+	deleteKeyIfValueMatches(container, "terminationMessagePolicy", "File")
+
+	return container
+}
+
+func cleanDeploymentTrigger(deploymentTrigger map[interface{}]interface{}) map[interface{}]interface{} {
+	if val, ok := deploymentTrigger["imageChangeParams"]; ok {
+		imageChangeParams := val.(map[interface{}]interface{})
+		// TODO: Verify this is the default
+		deleteKeyIfValueMatches(imageChangeParams, "automatic", true)
+		delete(imageChangeParams, "lastTriggeredImage")
+	}
+
+	return deploymentTrigger
+}
+
 // https://github.com/openshift/origin/blob/master/pkg/image/apis/image/types.go
 func cleanImageStream(imageStream map[interface{}]interface{}) map[interface{}]interface{} {
 	imageStream = cleanTemplateObject(imageStream)
@@ -133,12 +233,7 @@ func cleanImageStream(imageStream map[interface{}]interface{}) map[interface{}]i
 	// Tags seem to be ignored and recreated by the server
 	delete(imageStreamSpec, "tags")
 	deleteKeyIfEmpty(imageStream, "spec")
-
-	if val, ok := imageStream["status"]; ok {
-		imageStreamStatus := val.(map[interface{}]interface{})
-		deleteKeyIfValueMatches(imageStreamStatus, "dockerImageRepository", "")
-		deleteKeyIfEmpty(imageStream, "status")
-	}
+	delete(imageStream, "status")
 
 	return imageStream
 }
