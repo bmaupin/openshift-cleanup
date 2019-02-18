7f8ae5a987ebc632b7de50fcf9a538880cce39bb
bmaupin <bmaupin@users.noreply.github.com>
2019-02-18 09:22:11 -0500

    Add another list test and a JSON test

--- openshift-cleanup_test.go
+++ openshift-cleanup_test.go
@@ -18,6 +18,30 @@ func TestListCleanup1(t *testing.T) {
 	}
 }
 
+func TestListCleanupNodejsEx(t *testing.T) {
+	testContents, err := ioutil.ReadFile("testdata/openshift-list-nodejs-ex-cleaned.yaml")
+	if err != nil {
+		t.Errorf("Unexpected error reading test data file: %s", err)
+	}
+
+	cleanedContents := cleanOpenshiftConfigFile("testdata/openshift-list-nodejs-ex-original.yaml")
+	if bytes.Compare(cleanedContents, testContents) != 0 {
+		t.Error("Cleaned list contents don't match")
+	}
+}
+
+func TestListCleanupNodejsExJson(t *testing.T) {
+	testContents, err := ioutil.ReadFile("testdata/openshift-list-nodejs-ex-cleaned.yaml")
+	if err != nil {
+		t.Errorf("Unexpected error reading test data file: %s", err)
+	}
+
+	cleanedContents := cleanOpenshiftConfigFile("testdata/openshift-list-nodejs-ex-original.json")
+	if bytes.Compare(cleanedContents, testContents) != 0 {
+		t.Error("Cleaned list contents don't match")
+	}
+}
+
 func TestTemplateCleanup1(t *testing.T) {
 	testContents, err := ioutil.ReadFile("testdata/openshift-template-cleaned1.yaml")
 	if err != nil {
