1177f83e3719acc0f8391db89ffa24cad59e88c0
bmaupin <bmaupin@users.noreply.github.com>
2019-02-18 09:17:48 -0500

    .yaml seems to be more common than .yml

--- openshift-cleanup_test.go
+++ openshift-cleanup_test.go
@@ -7,36 +7,36 @@ import (
 )
 
 func TestListCleanup1(t *testing.T) {
-	testContents, err := ioutil.ReadFile("testdata/openshift-list-cleaned1.yml")
+	testContents, err := ioutil.ReadFile("testdata/openshift-list-cleaned1.yaml")
 	if err != nil {
 		t.Errorf("Unexpected error reading test data file: %s", err)
 	}
 
-	cleanedContents := cleanOpenshiftConfigFile("testdata/openshift-list-original1.yml")
+	cleanedContents := cleanOpenshiftConfigFile("testdata/openshift-list-original1.yaml")
 	if bytes.Compare(cleanedContents, testContents) != 0 {
 		t.Error("Cleaned list contents don't match")
 	}
 }
 
 func TestTemplateCleanup1(t *testing.T) {
-	testContents, err := ioutil.ReadFile("testdata/openshift-template-cleaned1.yml")
+	testContents, err := ioutil.ReadFile("testdata/openshift-template-cleaned1.yaml")
 	if err != nil {
 		t.Errorf("Unexpected error reading test data file: %s", err)
 	}
 
-	cleanedContents := cleanOpenshiftConfigFile("testdata/openshift-template-original1.yml")
+	cleanedContents := cleanOpenshiftConfigFile("testdata/openshift-template-original1.yaml")
 	if bytes.Compare(cleanedContents, testContents) != 0 {
 		t.Error("Cleaned template contents don't match")
 	}
 }
 
 func TestTemplateCleanup2(t *testing.T) {
-	testContents, err := ioutil.ReadFile("testdata/openshift-template-cleaned2.yml")
+	testContents, err := ioutil.ReadFile("testdata/openshift-template-cleaned2.yaml")
 	if err != nil {
 		t.Errorf("Unexpected error reading test data file: %s", err)
 	}
 
-	cleanedContents := cleanOpenshiftConfigFile("testdata/openshift-template-original2.yml")
+	cleanedContents := cleanOpenshiftConfigFile("testdata/openshift-template-original2.yaml")
 	if bytes.Compare(cleanedContents, testContents) != 0 {
 		t.Error("Cleaned template contents don't match")
 	}
