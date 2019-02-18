efa349b94d10c98e71329f75cb5bd853592df593
bmaupin <bmaupin@users.noreply.github.com>
2019-02-18 11:52:56 -0500

    Add another test
    This one's the same as template-2 but exported as a list

--- openshift-cleanup_test.go
+++ openshift-cleanup_test.go
@@ -18,6 +18,18 @@ func TestListCleanup1(t *testing.T) {
 	}
 }
 
+func TestListCleanup2(t *testing.T) {
+	testContents, err := ioutil.ReadFile("testdata/openshift-list-2-cleaned.yaml")
+	if err != nil {
+		t.Errorf("Unexpected error reading test data file: %s", err)
+	}
+
+	cleanedContents := cleanOpenshiftConfigFile("testdata/openshift-list-2-original.yaml")
+	if bytes.Compare(cleanedContents, testContents) != 0 {
+		t.Error("Cleaned list contents don't match")
+	}
+}
+
 func TestListCleanupNodejsEx(t *testing.T) {
 	testContents, err := ioutil.ReadFile("testdata/openshift-list-nodejs-ex-cleaned.yaml")
 	if err != nil {
